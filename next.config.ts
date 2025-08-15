3ef5ccf70cbe01a0158c81d249f156a486519421
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2025-08-15 16:08:07 +0530

    Build: Type Valdiation Fix

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,10 @@
+import type { NextConfig } from 'next';
+import webpack from 'webpack';
+import dotenv from 'dotenv';
+
+
 const nextConfig = {
+   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -13,7 +19,11 @@ const nextConfig = {
     // Tree-shake apprach amnd optimize server components.
     // Whether the server-side pkgs have any dependencies node api or build setups - when the nextjs can't automatically handle server component optimization
     // - To ensure nextjs bundles it properly in the server-side build process
-  }
+  },
+  webpack:(config) => {
+    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+    return config;
+  },
 }
 
 export default nextConfig;
