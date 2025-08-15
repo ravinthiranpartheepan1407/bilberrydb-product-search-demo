4858cbf1dfbadfc2ee5a3c7cdccfed97e177629a
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2025-08-15 15:38:32 +0530

    Build: Type Valdiation Fix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 const nextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   experimental: {
     serverComponentExternalPackages: ['bilberrydb'] 
+    // Usually nextjs app doesnt bundle nodejs pkgs into server components
+    // Criteria: 
+    // - Include this package when rendering on the server 
+    // Error Status: Build failed
+    // serverComponentExternalPackages module
+    // How it works?
+    // Tree-shake apprach amnd optimize server components.
+    // Whether the server-side pkgs have any dependencies node api or build setups - when the nextjs can't automatically handle server component optimization
+    // - To ensure nextjs bundles it properly in the server-side build process
   }
 }
 
