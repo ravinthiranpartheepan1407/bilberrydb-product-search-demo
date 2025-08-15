4af3a8fe91b5425e56d9297634f6f039796bd3f0
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2025-08-15 16:09:26 +0530

    Build: Type Valdiation Fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
+import type { Configuration as WebpackConfig } from 'webpack';
 import webpack from 'webpack';
 import dotenv from 'dotenv';
 
+dotenv.config();
 
 const nextConfig = {
    reactStrictMode: true,
@@ -20,8 +22,8 @@ const nextConfig = {
     // Whether the server-side pkgs have any dependencies node api or build setups - when the nextjs can't automatically handle server component optimization
     // - To ensure nextjs bundles it properly in the server-side build process
   },
-  webpack:(config) => {
-    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+  webpack: (config: WebpackConfig) => {
+    config.plugins?.push(new webpack.EnvironmentPlugin(process.env));
     return config;
   },
 }
